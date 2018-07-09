926270d1f5ca76852917b6351fb35c44015216eb
EC2 Default User <ec2-user@ip-172-31-6-69.us-east-2.compute.internal>
2018-07-09 06:08:02 +0000

    login function and templates

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+    public function index() {
+
+        $users = User::paginate(10);
+        
+        return view('users.index', [
+            'users' => $users,
+        ]);
+    }
+}
