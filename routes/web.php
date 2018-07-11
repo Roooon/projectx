335e2da2baa02ab930509e7d190854c75f527ee2
rei.kono <rei.kono@rakuten.com>
2018-07-11 07:32:42 +0000

    create timeline 1

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
-Route::get('/', 'TimelineController@index');
+Route::get('/', 'PostsController@index');
 
 Route::resource('profile','UserController');
+
+ Route::resource('posts', 'PostsController', ['only' => ['store', 'destroy']]);
