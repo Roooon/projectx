9dd342b069cad78d75064c17b753c0d80b818c1b
rei.kono <rei.kono@rakuten.com>
2018-07-11 01:18:49 +0000

    name change ron

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,6 +15,7 @@
         <div class="center jumbotron">
             <div class="text-center">
                 <h1>Welcome to ProjectX</h1>
+                <h1>Ryuchel is ron.</h1>
                 {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
             </div>
         </div>
