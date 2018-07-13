b6c586c42bdc0af54d3cbb3e92a239aa3651a349
irisz-kovacs <ec2-user@ip-172-31-17-78.us-east-2.compute.internal>
2018-07-13 05:26:19 +0000

    profile.show changed

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,1 +1,31 @@
-THIS IS MY PROFILE
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <aside class="col-xs-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">{{ $user->emil }}</h3>
+                </div>
+                <div class="panel-body">
+                    <img class="media-object img-rounded img-responsive" src="{{ Gravatar::src($user->email, 500) }}" alt="">
+                </div>
+            </div>
+            @include('buttons.follow_button', ['user' => $user])
+        </aside>
+        <div class="col-xs-8">
+           
+            @if (Auth::id() == $user->id)
+                  {!! Form::open(['route' => 'posts.store']) !!}
+                      <div class="form-group">
+                          {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'rows' => '2']) !!}
+                          {!! Form::submit('Post', ['class' => 'btn btn-primary btn-block']) !!}
+                      </div>
+                  {!! Form::close() !!}
+            @endif
+             @if (count($posts) > 0)
+                @include('posts.posts', ['posts' => $posts])
+            @endif
+        </div>
+    </div>
+@endsection
