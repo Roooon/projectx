fcf7c5d054412456a4597cad26f9a60a8b3ab459
irisz-kovacs <ec2-user@ip-172-31-17-78.us-east-2.compute.internal>
2018-07-13 08:05:18 +0000

    [fix] navbar

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,18 +10,48 @@ class UserController extends Controller
     //
     public function index() {
 
-        $users = \Auth::user();
+        $user = \Auth::user();
         
-        return view('profile.show', [
-            'users' => $users,
+        return view('profile.profile', [
+            'user' => $user,
         ]);
     }
     
     public function show($id) {
-        $users = \Auth::user();
-        
+        $user = \Auth::user();
         return view('profile.show', [
-            'users' => $users,
+            'user' => $user,
         ]);
 
-}}
+}
+    public function user_follows($id)
+    {
+        $user = User::find($id);
+        $follows = $user->user_follows()->paginate(10);
+
+        $data = [
+            'user' => $user,
+            'users' => $follows,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('profile.user_follows', $data);
+    }
+
+    public function followers($id)
+    {
+        $user = User::find($id);
+        $followers = $user->followers()->paginate(10);
+
+        $data = [
+            'user' => $user,
+            'users' => $followers,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('profile.followers', $data);
+}   
+    
+}
