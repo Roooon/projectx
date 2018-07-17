4804f1fbecb570d757509a777a9f2cba014af378
irisz-kovacs <ec2-user@ip-172-31-17-78.us-east-2.compute.internal>
2018-07-17 03:26:07 +0000

    ranamed intro table to intros table

--- database/migrations/2018_07_09_121315_create_intros_table.php
+++ database/migrations/2018_07_09_121315_create_intros_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateIntroTable extends Migration
+class CreateIntrosTable extends Migration
 {
     /**
      * Run the migrations.
