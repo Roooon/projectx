4abc3212f9eac72de7d7b22a5fb66dae9a357a37
EC2 Default User <ec2-user@ip-172-31-6-69.us-east-2.compute.internal>
2018-07-17 01:35:47 +0000

    sikatosite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,50 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
+    @if (Auth::check())
         <div class="row">
             <aside class="col-md-4">
             </aside>
             <div class="col-xs-8">
-@if (count($users) > 0)
-    <ul class="media-list">
-    @foreach ($users as $user)
-    
-
-    @foreach ($user->intro as $intro)
-    <li class="media">
-            <div class="media-left">
+                 
+                  
+                
             </div>
-        <div class="media-body">
-            <div>
-                {!! link_to_route('profile.profile', $user->name, ['id' => $user->id]) !!} <span class="text-muted">usered at {{ $user->created_at }}</span>
-            </div>
-            <div>
-                <p>{!! nl2br(e($intro->content)) !!}</p>
-            </div>
-            <div>
-                @if (Auth::id() == $user->user_id)
-                    {!! Form::open(['route' => ['users.destroy', $user->id], 'method' => 'delete']) !!}
-                        {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-xs']) !!}
-                    {!! Form::close() !!}
-                @endif
-            </div>
-           
         </div>
-    </li>
-    
-    @endforeach
-   
-@endforeach
-</ul>
-    </div>
-</div>
-@else
-<div class="center jumbotron">
-    <div class="text-center">
-        <h1>小さいスキルを世界へ</h1>
-        <h2>ProjectXへようこそ</h2>
-        {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
-    </div>
-</div>
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>小さいスキルを世界へ</h1>
+                <h2>ProjectXへようこそ</h2>
+                {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+        </div>
     @endif
 @endsection
