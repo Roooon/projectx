aa91322964c605369434a546482e5b84f6585a6a
irisz-kovacs <ec2-user@ip-172-31-17-78.us-east-2.compute.internal>
2018-07-20 09:06:05 +0000

    desgin working!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,12 +19,23 @@
 </div>
 
 @else
-<div class="center jumbotron">
     <div class="text-center">
-        <h1>小さいスキルを世界へ</h1>
-        <h2>ProjectXへようこそ</h2>
-        {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
+        <h1>知らなかった、あの人のコト</h1>
+        <!--<h3>通知表に怯えてた幼少期。自己分析で必死だった就活。毎日伺う上司の顔色。インスタ映え。</h3>-->
+        <h2>友人があなた、あなたが友人をシェアするSNSの新しいカタチ。</h2>
+        
+        <div class="row botan">
+            
+        <div class="col-sm-6">{!! link_to_route('signup.get', 'SIGN UP', null, ['class' => 'newyorkbtns']) !!}</div>
+        <div class="col-sm-6">{!! link_to_route('login', 'LOGIN', null, ['class' => 'newyorkbtnl']) !!}</div>
+        </div>
+        
+        <img alt="More" class="yazirusi" src="https://de7iszmjjjuya.cloudfront.net/assets/about/root/ic_chevron_bottom-3df843a38d23002d647276c1ff79901a834bd926f74e81ae4753e4565483b577.png" width="24" height="24">
+        
+        <p>
+            MORE ABOUT
+        </p>
+       
     </div>
-</div>
 @endif
 @endsection
