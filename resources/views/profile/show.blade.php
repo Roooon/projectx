94111fa4479a28b6d36e909da0470e4c2a6c2043
EC2 Default User <ec2-user@ip-172-31-10-4.us-east-2.compute.internal>
2018-07-23 01:14:36 +0000

    modify mypage/css etc

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -4,15 +4,19 @@
 @section('content')
     <div class="row">
         <div class="bottons">
-        
-        {!! link_to_route('postintro.create','Write intro' ,['id' => $user->id]) !!}
-        {!! link_to_route('skills.create','Add skill' ,['id' => $user->id]) !!}
-        {!! link_to_route('users.follows','follows' ,['id' => $user->id]) !!}
-        {!! link_to_route('users.followers','followers' ,['id' => $user->id]) !!}
-
+            <div class="col-sm-8">
+            <a class="btn btn-info btn-lg" href="{{ route('postintro.create', ['id' => $user->id]) }}">Write intro</a>
+            <a class="btn btn-info btn-lg" href="{{ route('skills.create', ['id' => $user->id]) }}">Add skill</a>
+            </div>
+            <div class="col-sm-8">
+            <div class="btnaf">
+            <a class="btn btn-info btn-lg" href="{{ route('users.follows', ['id' => $user->id]) }}">follows</a>
+            <a class="btn btn-info btn-lg" href="{{ route('users.followers', ['id' => $user->id]) }}">followers</a>
+            </div>
+            </div>
         </div>
-
-        <aside class="col-xs-4">
+              <div class="logo">
+        <div class="col-xs-4">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h2 class="panel-title">{{ $user->email }}</h2>
@@ -25,7 +29,8 @@
             
             
             <h3>Self introduction</h3>
-        </aside>
+        </div>
+        </div>
         <div class="skill_intro">
             <div class="col-xs-8">
            <ul class="nav nav-tabs nav-justified">
