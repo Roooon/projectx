067df54dd6d3521e1f62a473120c63df0050d37d
EC2 Default User <ec2-user@ip-172-31-3-76.us-east-2.compute.internal>
2018-07-24 06:06:57 +0000

    serch modification

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -22,7 +22,9 @@
               <li class="nav-serch">
               <form class="form-inline" action="/search" method="get">
                     <input class="form-control mr-sm-2" type="text" placeholder="User Search" aria-label="Search" name='keyword'>
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    
+                    <input type="image" src="images/btn2.gif" alt="検索" name="search" class="search" />
+                    
               </form>
               </li>
               
